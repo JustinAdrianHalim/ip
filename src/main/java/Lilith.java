26245b5ca19351542275335f7976401f06b02150
Justin Adrian Halim <e1155743@u.nus.edu>
2026-01-29 23:56:13 +0800

    Level-3

--- src/main/java/Lilith.java
+++ src/main/java/Lilith.java
@@ -6,33 +6,68 @@ public class Lilith {
         System.out.println("Hello, I'm Lilith!");
         System.out.println("Would you like a strawberry cake?");
 
-        planner();
-    }
-
-
-    public static void planner() {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> Tasklist = new ArrayList<>();
+        ArrayList<Task> tasklist = new ArrayList<>();
+        tasklist.add(new Task("a"));
+        tasklist.add(new Task("b"));
 
         while (true) {
             System.out.println("--------------------------------------------------------------");
             String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Bye-bye! I will always be here when you need me!");
                 break;
             }
-            if (input.equalsIgnoreCase("list")) {
-                for (int i = 0; i < Tasklist.size(); i++){   
-                    System.out.println((i+1) + ". " + Tasklist.get(i));
+            else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < tasklist.size(); i++){   
+                    System.out.println((i+1) + ". " + tasklist.get(i));
                 }
             }
 
+            else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasklist.get(index).mark();
+                System.out.println("Nicely done! Good job!");
+                System.out.println(tasklist.get(index));
+
+            }
+
+            else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasklist.get(index).unmark();
+                System.out.println("Make sure to finish it soon, ok?");
+                System.out.println(tasklist.get(index));
+            }
+
             else{
-                Tasklist.add(input);
+                Task taskname = new Task(input);
+                tasklist.add(taskname);
                 System.out.println("added:" + input);
             }
         }
         scanner.close();
+    }
+}
+
+class Task{
+    String taskname;
+    boolean status;
+
+    Task(String taskname){
+        this.taskname = taskname;
+        this.status = false;
+    }
+
+    void mark(){
+        status = true;
+    }
+
+    void unmark(){
+        status = false;
+    }
+    @Override
+    public String toString() {
+        return "[" + (status ? "X" : " ") + "] " + taskname;
     }
 }
