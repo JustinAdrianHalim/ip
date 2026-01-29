6f79477a787f0ec8bc39c6afe8b25033c0b5ba86
Justin Adrian Halim <e1155743@u.nus.edu>
2026-01-29 16:58:01 +0800

    Level-0

---
+++ src/main/java/Lilith.java
@@ -0,0 +1,11 @@
+public class Lilith {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Lilith!");
+        System.out.println("Would you like a strawberry cake?");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
+
