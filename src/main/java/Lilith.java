69a234a8d463936fd683fe74911c09227e27ac5f
Justin Adrian Halim <e1155743@u.nus.edu>
2026-01-29 17:09:30 +0800

    Level-1

--- src/main/java/Lilith.java
+++ src/main/java/Lilith.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 public class Lilith {
 
     public static void main(String[] args) {
         System.out.println("Hello, I'm Lilith!");
         System.out.println("Would you like a strawberry cake?");
-        System.out.println("--------------------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        echo();
+    }
+
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("--------------------------------------------------------------");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+        scanner.close();
     }
 }
-
-
